8157bb85c985c4d6e8b4b0bcb8c6ce12e815d961
Juan <morenostudent1@gmail.com>
2022-06-25 09:28:33 -0700

    primero commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'nucj67rj8pyg'
+
+const App = () => {
+    return (
+      <div className="app_wrapper">
+            <Chat>
+                
+        </Chat>
+      </div>
+    )
+}
+
+export default App
